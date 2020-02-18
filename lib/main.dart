907d8d531db19cfc6db4664c86184a54b554f1c9
dikaipan <handikajuliansyah.hj@gmail.com>
2020-02-18 20:44:47 +0700

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:tab_bar_flutter/home.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,8 +10,37 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Home(),
+    return MaterialApp(
+      //we use the DefaultTabController from Flutter
+      home: DefaultTabController(
+        //tabbar length that we used
+        length: 5,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              //we need to initialization the tabbar too
+              tabs: [
+                Tab(icon: Icon(Icons.directions_car)),
+                Tab(icon: Icon(Icons.directions_transit)),
+                Tab(icon: Icon(Icons.whatshot)),
+                Tab(icon: Icon(Icons.transfer_within_a_station)),
+                Tab(icon: Icon(Icons.accessibility_new)),
+              ],
+            ),
+            title: Text('My TabBar'),
+          ),
+          // and then we got the body to call with tabbarview widgets
+          body: TabBarView(
+            children: [
+              Icon(Icons.directions_car),
+              Icon(Icons.directions_transit),
+              Icon(Icons.whatshot),
+              Icon(Icons.transfer_within_a_station),
+              Icon(Icons.accessibility_new),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
